09eaaf1a56643d28d455a81e034d21a5aa7c1c46
Maksim Golitsyn <maxim@infofree.ru>
2018-10-21 15:21:52 +0500

    Escaping inputs with React

--- security-examples/components/EscapeInput.js
+++ security-examples/components/EscapeInput.js
@@ -1,1 +1,16 @@
+import React, { Component } from 'react';
 
+const inputString = "<input type='text' defaultValue='rendered' />";
+
+class EscapeInput extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Safe React Input</h3>
+                <div>{inputString}</div>
+            </div>
+        );
+    }
+}
+
+export default EscapeInput;
--- security-examples/index.js
+++ security-examples/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import EscapeInput from './components/EscapeInput';
+
 render(
-    <div>Security in React</div>,
+    <div>
+        <hr />
+        <EscapeInput />
+    </div>,
     document.getElementById('root')
 );
