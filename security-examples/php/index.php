b42d3520df53b412a2d359c29e43b3d80aeb3203
Maksim Golitsyn <maxim@infofree.ru>
2018-10-21 15:12:39 +0500

    Tests XSS via PHP

--- security-examples/php/index.php
+++ security-examples/php/index.php
@@ -3,12 +3,15 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>React Security Examples</title>
+    <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-    <h1>React Security Examples</h1>
-    <div id="root"></div>
-    <script src="index.js"></script>
+    <h3>Vulnerable Input</h3>
+    <form action="">
+        <input name="field" type="text" />
+        <input type="submit" />
+        <h4>Result: <?= $_GET['field'] ?></h4>
+    </form>
 </body>
 </html>
