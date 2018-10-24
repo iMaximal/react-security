3fa486f2e9837240a1ac8e358408e262d642f849
Maksim Golitsyn <maxim@infofree.ru>
2018-10-24 19:33:25 +0500

    Added Auth component and logic

--- authentication/src/components/App.js
+++ authentication/src/components/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const App = () => (
+import Login from './Login';
+import Logout from './Logout';
+
+const Home = (props) => (
     <div>
         <h2>Foddie Hun</h2>
         <div><Link to='/ramen'>Ramen</Link></div>
         <div><Link to='/sushi'>Sushi</Link></div>
+        <br />
+        <Logout {...props} />
+    </div>
+);
+
+const App = (props) => (
+    <div>
+        {
+            props.auth.isAuthenticated() ? <Home {...props} /> : <Login {...props} />
+        }
     </div>
 );
 
--- authentication/src/index.js
+++ authentication/src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import history from './history';
-import { Router, Switch, Route } from 'react-router-dom';
 import { App, Ramen, Sushi } from './components'; 
+import Auth from './Auth';
 import './../index.css';
+
+const auth = new Auth();
+
+const callbackComponent = props => {
+    if (props.location.hash.includes('access_token')) {
+      setTimeout(() => auth.handleAuthentication());
+      return <h4>loading...</h4>;
+    } else {
+      return <Redirect to={{ pathname: '/' }} />
+    }
+  };
 
 render(
     <Router history={history}>
         <Switch>
-            <Route exact path='/' component={App} />
+            <Route exact path='/' render={() => <App auth={auth} />} />
+            <Route path='/callback' render={props => callbackComponent(props)} />
             <Route path='/ramen' component={Ramen} />
             <Route path='/sushi' component={Sushi} />
         </Switch>
