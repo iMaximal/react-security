c2d39439e7fb92fcd1b046b0ef7d1b51c2892ec5
Maksim Golitsyn <maxim@infofree.ru>
2018-10-27 16:34:16 +0500

    Refactored, added session

--- fullstack_auth/backend/app.js
+++ fullstack_auth/backend/app.js
@@ -1,23 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const pool = require('./db');
+
+const user = require('./user');
 
 const app = express();
 
 app.use(bodyParser.json());
-
-app.get('/user/all', (req, res, next) => {
-    pool.query('SELECT * FROM users', (q_err, q_res) => {
-        if (q_err) return next(q_err);
-
-        res.json(q_res.rows);
-    })
-});
-
-app.post('/user/new', (req, res, next) => {
-    console.log(req.body);
-    res.json(req.body);
-});
+app.use('/user', user);
 
 app.use((err, req, res, next) => {
     if (!err.statusCode) err.statusCode = 500;
--- fullstack_auth/backend/users.sql
+++ fullstack_auth/backend/users.sql
@@ -1,9 +1,9 @@
 CREATE TABLE users(
-  username character(64),
-  password character(64),
+  username_hash character(64),
+  password_hash character(64),
   session_id character(36)
 );
 
-INSERT INTO users(username, password) VALUES
+INSERT INTO users(username_hash, password_hash) VALUES
 ('foo123', 'pass123'),
 ('bar456', 'word456');
