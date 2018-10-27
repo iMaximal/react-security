6ca289e7c557b1171dafd93048b4da5ec11c3a43
Maksim Golitsyn <maxim@infofree.ru>
2018-10-27 18:57:40 +0500

    Added set_session_cookie and set_session

--- fullstack_auth/backend/user.js
+++ fullstack_auth/backend/user.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 
 const pool = require('./db');
-const { hash } = require('./helper');
+const { hash, Session } = require('./helper');
 
 const router = new Router();
 
@@ -13,36 +13,74 @@ router.get('/all', (req, res, next) => {
     })
 });
 
-router.post('/new', (req, res, next) => {
+const set_session_cookie = (session_str, res) => {
+    res.cookie('session_str', session_str, {
+      expire: Date.now() + 3600000,
+      httpOnly: true,
+      secure: process.env.MODE == 'production' // use with https for a secure cookie
+    });
+  }
+
+const set_session = (username, res, session_id) => {
+    let session, session_str;
+  
+    if (session_id) {
+      session_str = Session.dataToString(username, session_id);
+    } else {
+      session = new Session(username);
+      session_str = session.toString();
+    }
+  
+    return new Promise((resolve, reject) => {
+      if (session_id) {
+        set_session_cookie(session_str, res);
+        resolve();
+      } else {
+        pool.query(
+          'UPDATE users SET session_id = $1 WHERE username_hash = $2',
+          [session.id, hash(username)],
+          (q_err, q_res) => {
+            if (q_err) return reject(q_err);
+            set_session_cookie(session_str, res);
+            resolve();
+          }
+        );
+      }
+    });
+  };
+
+  router.post('/new', (req, res, next) => {
     const { username, password } = req.body;
     const username_hash = hash(username);
-
+  
     pool.query(
-        'SELECT * FROM users WHERE username_hash = $1',
-        [username_hash],
-        (q0_err, q0_res) => {
-            if  (q0_err) return next(q0_err);
-
-            if (q0_res.rows.length === 0) {
-                // insert a new user 
-                pool.query(
-                    'INSERT INTO users(username_hash, password_hash) VALUES($1, $2)',
-                    [username_hash, hash(password)],
-                    (q1_err, q1_res) => {
-                        if (q1_err) return next(q1_err);
-                        res.json({
-                            msg: 'Successfully created user!'
-                        });
-                    }
-                )
-            } else {
-                res.status(409).json({ 
-                    type: 'error',
-                    msg: 'This username has been taken'
+      'SELECT * FROM users WHERE username_hash = $1',
+      [username_hash],
+      (q0_err, q0_res) => {
+        if (q0_err) return next(q0_err);
+  
+        if (q0_res.rows.length === 0) {
+          pool.query(
+            'INSERT INTO users(username_hash, password_hash) VALUES($1, $2)',
+            [username_hash, hash(password)],
+            (q1_err, q1_res) => {
+              if (q1_err) return next(q1_err);
+  
+              set_session(username, res)
+                .then(() => {
+                  res.json({ msg: 'Successfully created user!' });
                 })
+                .catch(error => next(error));
             }
+          )
+        } else {
+          res.status(409).json({
+            type: 'error',
+            msg: 'This username has been taken'
+          });
         }
+      }
     )
-});
+  });
 
 module.exports = router;
